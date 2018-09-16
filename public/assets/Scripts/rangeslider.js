860b6180322541656d6d02c634f7f51e87831d65
gurpreet <gurpreet.cigs@gmail.com>
2018-09-16 20:13:56 +0530

    Fixes

--- public/assets/Scripts/rangeslider.js
+++
@@ -1,5 +0,0 @@
-new Dragdealer('just-a-slider', {
-                animationCallback: function(x, y) {
-                  $('#just-a-slider .value').text(Math.round(x * 100));
-                }
-              });
